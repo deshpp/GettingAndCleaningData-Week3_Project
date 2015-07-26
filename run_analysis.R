13ee42706215b3c2385cb42a3c105c48625a7c02
prasadmd83 <prasadmd83@users.noreply.github.com>
2015-07-26 14:07:53 +0100

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -1,10 +1,11 @@
-## Create one R script called run_analysis.R that does the following:
-## 1. Merges the training and the test sets to create one data set.
+## The R script works as below:
+## 1. Merges the training and the test sets to create as single data set.
 ## 2. Extracts only the measurements on the mean and standard deviation for each measurement.
 ## 3. Uses descriptive activity names to name the activities in the data set
 ## 4. Appropriately labels the data set with descriptive activity names.
 ## 5. Creates a second, independent tidy data set with the average of each variable for each activity and each subject.
 
+##if required packages are not present then install them
 if (!require("data.table")) {
   install.packages("data.table")
 }
@@ -16,16 +17,19 @@ if (!require("reshape2")) {
 require("data.table")
 require("reshape2")
 
-# Load: activity labels
+#set working directory where the data is present - note the data is already downloaded and unzipped to avoid extra coding
+setwd("C:\\R\\GetCleanData\\getdata-projectfiles-UCI HAR Dataset")
+
+# Load data: activity labels
 activity_labels <- read.table("./UCI HAR Dataset/activity_labels.txt")[,2]
 
-# Load: data column names
+# Load data: data column names
 features <- read.table("./UCI HAR Dataset/features.txt")[,2]
 
 # Extract only the measurements on the mean and standard deviation for each measurement.
 extract_features <- grepl("mean|std", features)
 
-# Load and process X_test & y_test data.
+# Process X_test & y_test data.
 X_test <- read.table("./UCI HAR Dataset/test/X_test.txt")
 y_test <- read.table("./UCI HAR Dataset/test/y_test.txt")
 subject_test <- read.table("./UCI HAR Dataset/test/subject_test.txt")
